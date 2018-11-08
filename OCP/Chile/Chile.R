f6d07ec45c4f810a9dab30c3f51aa25c2d7f57d3
govtransparency <42810041+govtransparency@users.noreply.github.com>
2018-11-08 15:41:13 +0100

    Add files via upload

--- OCP/Chile/Chile.R
+++ OCP/Chile/Chile.R
@@ -1,7 +1,7 @@
 #Chile
 
 #set new directory for selected files to merge
-setwd("C:/Users/Bertold/Downloads/2018-09-20-chile")
+setwd("path")
 getwd()
 
 #setting a bigger memory
@@ -13,27 +13,31 @@ library(readr)
 df_chile <- read_csv('2018-09-20-chile.csv')
 
 #alternatively with Latin-1 encoding
-df_chile <- read.csv("C:/Users/Bertold/Downloads/2018-09-20-chile/2018-09-20-chile.csv", header = TRUE, sep = ",", encoding = "Latin-1")
+df_chile <- read.csv("C:/Users/Bertold/Downloads/2018-09-20-chile/2018-10-25-digiwhist-cl.csv", header = TRUE, sep = ",", encoding = "Latin-1")
 
 
 #check missings
 sapply(df_chile, function(df_chile) sum(is.na(df_chile)))
 #most of the variables are irrelevant and are completely miising, remove them
 
+#replace 0, empty cells with NA
+df_chile[df_chile == ""] <- NA
+
 #remove columns with no or almost 0 values (missing rate is 80% or higher) 
 df_chile <- df_chile[, colSums(is.na(df_chile)) != nrow(df_chile)]
 df_chile <- df_chile[colSums(is.na(df_chile))/nrow(df_chile) < .8]
 names(df_chile)
 
+
 #remove additional unnecessary variables
-df_chile <- df_chile[,-c(4,29,30,35,36,37,38,39,40,41,42)]
-df_chile2018$tender_documents_count <- NULL
+df_chile <- df_chile[,-c(4,7,18,19,32,34,35,36,37,38,39,40,41)]
+#df_chile$tender_documents_count <- NULL
 
 #check missing values
 sapply(df_chile, function(df_chile) sum(is.na(df_chile)))
 
 #check years distribution
-table(year)
+table(df_chile$tender_year)
 #mainly from 2018
 
 #subset a sample with tenders from 2018
@@ -43,12 +47,14 @@ library(dplyr)
 df_chile2018 <- filter(df_chile, df_chile$tender_year == "2018")
 #185285 observations
 
-#replace 0 values with NA
-df_chile2018[df_chile2018 == 0] <- NA
 
 #check types of variables
 sapply(df_chile2018, class)
-#mostly ok, change prices to numeric
+
+cols <- c(2,3,6,7,14,17,18,21,25,26,27,31)
+df_chile2018[,cols] = lapply(df_chile2018[,cols], as.character)
+
+df_chile2018$tender_publications_firstdcontractawarddate <- as.Date(df_chile2018$tender_publications_firstdcontractawarddate)
 
 df_chile2018$tender_estimatedprice <- as.numeric(df_chile2018$tender_estimatedprice)
 df_chile2018$lot_estimatedprice <- as.numeric(df_chile2018$lot_estimatedprice)
@@ -57,17 +63,20 @@ df_chile2018$bid_price <- as.numeric(df_chile2018$bid_price)
 
 #rename variables to OCDS
 library(data.table)
-setnames(df_chile2018, old = c('tender_id','tender_title', 'tender_proceduretype', 'tender_lotscount', 'tender_recordedbidscount', 'tender_cpvs', 'tender_maincpv', 'tender_awardcriteria_count','tender_estimatedprice','tender_finalprice','lot_estimatedprice','bid_price','tender_corrections_count','lot_title','buyer_id','buyer_masterid','buyer_name','buyer_email','buyer_contactname','buyer_city','bidder_id','bidder_masterid','bidder_name','bid_iswinning','award_count','notice_count','tender_publications_firstdcontractawarddate','tender_year','savings','tender_description_length','currency','ID','ca_id'), new = c('ten_id', 'ten_title','ten_procMethod','ten_item_quantity','ten_bids','ten_item_class_id','dropcpv','ten_aw_crit_count', 'ten_plan_vamount', 'ten_vamount', 'ten_item_plan_vamount', 'ten_item_unit_vamount', 'ten_am_count', 'ten_item_descr', 'buyer_id', 'buyer_addid', 'buyer_name', 'dropemail', 'dropconname', 'buyer_add_city', 'ten_tenderers_id', 'ten_tenderers_addid', 'ten_tenderers_name', 'aw_status', 'aw_count', 'notice_count', 'aw_date', 'ten_year', 'savings', 'ten_descr_length', 'ten_vcurr', 'ID', 'ca_id'))
+setnames(df_chile2018, old = c('tender_id','tender_title', 'tender_proceduretype', 'tender_lotscount', 'tender_recordedbidscount', 'tender_cpvs', 'tender_maincpv', 'tender_awardcriteria_count','tender_estimatedprice','tender_finalprice','lot_estimatedprice','bid_price','tender_corrections_count','lot_title','buyer_id','buyer_masterid','buyer_name','buyer_city','bidder_id','bidder_masterid','bidder_name','bid_iswinning','award_count','notice_count','tender_publications_firstdcontractawarddate','tender_year','savings','currency'), new = c('ten_id', 'ten_title','ten_procMethod','ten_item_quantity','ten_bids','ten_item_class_id','tender_cpvs???','ten_aw_crit_count', 'ten_plan_vamount', 'ten_vamount', 'ten_item_plan_vamount', 'ten_item_unit_vamount', 'ten_am_count', 'ten_item_descr', 'buyer_id', 'buyer_addid', 'buyer_name', 'dropemail', 'dropconname', 'buyer_add_city', 'ten_tenderers_id', 'ten_tenderers_addid', 'ten_tenderers_name', 'aw_status', 'aw_count', 'notice_count', 'aw_date', 'ten_year', 'savings', 'ten_descr_length', 'ten_vcurr', 'ID', 'ca_id'))
 names(df_chile2018)
-
-#drop unnecessary variables
-df_chile2018$dropcpv <- NULL
-df_chile2018$dropemail <- NULL
-df_chile2018$dropconname <- NULL
 
 
 #check missing values
 sapply(df_chile2018, function(df_chile2018) sum(is.na(df_chile2018)))
+
+length(which(df_chile2018$bid_price == 0))
+length(unique(df_chile2018$tender_id))
+mean(is.na(df_chile2018$bid_price))
+
+table(df_chile2018$tender_proceduretype)
+table(df_chile2018$bid_iswinning)
+
 
 mean(is.na(df_chile2018$ten_procMethod))
 #0.4%
@@ -122,6 +131,7 @@ length(unique(df_chile2018$ca_id))
 
 df_chile2018$ten_id1 <- NULL
 
+df_chile2018$tender_cpvs <- gsub('[[:punct:]]', "", df_chile2018$tender_cpvs)
 
 #remove outliers from bid price
 df_chile2018$ten_vamount[df_chile2018$ten_vamount < 237836] <- NA
@@ -136,4 +146,4 @@ table(df_chile2018$ten_procMethod)
 #mostly open
 
 #save file
-write.csv(df_chile2018, file = "df_chile2018.csv")
+write.csv(df_chile2018,"path/filename.csv")
